9de8d773a934b5d6096dbf94ad315f5b5d2a9c47
Qu33nArlene <ahaskins93@yahoo.com>
2022-05-12 16:34:47 -0400

    api functionality fully functional

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
-//set variables and api URLs
-var searchedCity = $("#searched-city").val();
 var searchBtn = $("#searchBtn");
-var lat;
-var lon;
-var weatherInfoApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
 
 //set array for previously searched cities
 var savedCities = [];
 
 //fetch api for weather info
 searchBtn.on("click", function () {
-    console.log($("#searched-city").val);
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +
-        $("#searched-city").val() +
-        "&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c")
-        .then(function (response) {
+  var searchedCity = $("#searched-city").val();
+  fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      searchedCity +
+      "&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c"
+  ).then(function (response) {
+    if (response.ok) {
+      response
+        .json()
+        .then(function (data) {
+          return data;
+        })
+        .then(function (data) {
+          fetch(
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+              data[0].lat +
+              "&lon=" +
+              data[0].lon +
+              "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900c"
+          ).then(function (response) {
             if (response.ok) {
-                response.json().then(function (data) {
-                    lat = data[0].lat;
-                    lon = data[0].lon;
-                });
-            } else if (searchedCity === "") {
-                alert("please enter a city");
+              response.json().then(function (data) {
+                console.log(data);
+              });
             }
-        }).then(function () {
-            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=cf435fe1ad8c50ef7b1fe99bc009900c")
-                .then(function (response) {
-                    if (response.ok) {
-                        response.json().then(function (data) {
-                            console.log(data);
-                        })
-                    }
-
-                })
+          });
         });
-    })
+    } else if (searchedCity === "") {
+      alert("please enter a city");
+    }
+  });
+});
