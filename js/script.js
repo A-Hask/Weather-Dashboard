6b5fe63778b68b7b65d1466e9915f845c176a20f
Qu33nArlene <ahaskins93@yahoo.com>
2022-05-12 15:29:54 -0400

    testing

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 //set variables and api URLs
-var searchedCity = document.querySelector("#searched-city");
-var searchBtn = document.querySelector("#searchBtn");
-var latLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
-var weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900";
+var searchedCity = $("#searched-city").val();
+console.log(searchedCity);
+var searchBtn = $("#searchBtn");
+var latLonApi = "http://api.openweathermap.org/geo/1.0/direct?q=miami&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
+// var weatherInfoApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900";
 
 //set array for previously searched cities
 var savedCities = [];
 
 //fetch api for weather info
-searchBtn.on("click", function latLon() {
-    fetch(latLon).then(function(response){
-    if (response.ok) {
-            response.json().then(function() {
-            console.log(searchedCity);
+searchBtn.on("click", function (e) {
+    e.preventDefault();
+    fetch(latLonApi).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
             });
-        } else if (searchedCity.value === "") {
+        } else if (searchedCity === "") {
             alert("please enter a city");
         }
 
