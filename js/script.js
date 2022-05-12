63037eef294ebc49bacfbae2a67af64e6cff9d48
Qu33nArlene <ahaskins93@yahoo.com>
2022-05-12 13:19:23 -0400

    got fetch function to work kinda

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-//set variables for searched city and call api
-var searchedCity = ;
-var lat = ;
-var lon = ;
-//
+//set variables and api URLs
+var searchedCity = document.querySelector("#searched-city");
+var searchBtn = document.querySelector("#searchBtn");
+var latLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
+var weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900";
+
+//set array for previously searched cities
 var savedCities = [];
-var weatherInfo = function() {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900c");
-}
+
+//fetch api for weather info
+searchBtn.on("click", function latLon() {
+    fetch(latLon).then(function(response){
+    if (response.ok) {
+            response.json().then(function() {
+            console.log(searchedCity);
+            });
+        } else if (searchedCity.value === "") {
+            alert("please enter a city");
+        }
+
+    })
+});
+
