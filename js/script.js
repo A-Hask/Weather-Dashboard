163d6b54d6561982bb6ba453746a8720545f11a4
Qu33nArlene <ahaskins93@yahoo.com>
2022-05-12 15:59:57 -0400

    fixed functionality of api fetch requests

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 //set variables and api URLs
 var searchedCity = $("#searched-city").val();
-console.log(searchedCity);
 var searchBtn = $("#searchBtn");
-var latLonApi = "http://api.openweathermap.org/geo/1.0/direct?q=miami&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
-// var weatherInfoApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=cf435fe1ad8c50ef7b1fe99bc009900";
+var lat;
+var lon;
+var weatherInfoApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=cf435fe1ad8c50ef7b1fe99bc009900c";
 
 //set array for previously searched cities
 var savedCities = [];
 
 //fetch api for weather info
-searchBtn.on("click", function (e) {
-    e.preventDefault();
-    fetch(latLonApi).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            });
-        } else if (searchedCity === "") {
-            alert("please enter a city");
-        }
+searchBtn.on("click", function () {
+    console.log($("#searched-city").val);
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +
+        $("#searched-city").val() +
+        "&limit=5&appid=cf435fe1ad8c50ef7b1fe99bc009900c")
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    lat = data[0].lat;
+                    lon = data[0].lon;
+                });
+            } else if (searchedCity === "") {
+                alert("please enter a city");
+            }
+        }).then(function () {
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=cf435fe1ad8c50ef7b1fe99bc009900c")
+                .then(function (response) {
+                    if (reponse.ok) {
+                        response.json().then(function (data) {
+                            console.log(data);
+                        })
+                    }
 
+                })
+        });
     })
-});
-
