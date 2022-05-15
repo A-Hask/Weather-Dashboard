d200ce25325470b6c17fd759b149ff208aa36bc3
Qu33nArlene <ahaskins93@yahoo.com>
2022-05-15 18:10:23 -0400

    Added footer and made a few style adjustments

--- js/script.js
+++ js/script.js
@@ -10,16 +10,17 @@ var cityButton = $("#city-btn");
 //display 5-day weather forecast (date, weather icon, high temp, low temp, wind, humidity)
 var weatherInfo = function (weatherData, city) {
     currentWeather.empty();
-    currentWeather.append("<li>The weather in " + city + " today: "
-        + weatherData.daily[0].weather[0].description + ".<br> The high is "
+    currentWeather.append("<li>The weather in " + city + " today (" + moment().format("MMM DD, YYYY") + "): " 
+        + weatherData.daily[0].weather[0].description 
+        + ".<br> The high is "
         + weatherData.daily[0].temp.max + " degrees fahrenheit. <br> The low is "
         + weatherData.daily[0].temp.min + " degrees fahrenheit. <br> There will be an average of "
         + weatherData.current.humidity + "% humidity and the UV index will be at <span id='color-code'>" + weatherData.daily[0].uvi
         + "</span>.</li>");
     forecast.empty();
-    forecast.append("<li>" + city + "</li><br>");
+    forecast.append("<h3>" + city + "</h3><br>");
     for (var i = 1; i < 6; i++) {
-        forecast.append("<li class='col-6 row'>On " + moment().add(i, 'days').format("MMM DD, YYYY") + ", there will be " + weatherData.daily[i].weather[0].description + "."
+        forecast.append("<li class='col-12 row'>On " + moment().add(i, 'days').format("MMM DD, YYYY") + ", there will be " + weatherData.daily[i].weather[0].description + "."
             + "<br> The high will be " + weatherData.daily[i].temp.max + " degrees fahrenheit. <br> The low will be "
             + weatherData.daily[i].temp.min + " degrees fahrenheit. <br> Humidity will be at "
             + weatherData.daily[i].humidity + "% and wind speed will be "
@@ -88,7 +89,7 @@ function search() {
     });
     //save data to local storage
     var savedCity = JSON.parse(localStorage.getItem("savedCities"));
-    //empty search bar
+    //future feature: empty search bar
 
     //display previously searched cities. user must be able to click on cities to search again.
     saved.append("<button class='btn' id='city-btn'>" + searchedCity + "</button><br>");
@@ -101,17 +102,8 @@ searchBtn.on("click", function(){
 })
 
 cityButton.on("click", function(e){
-    search(e.target.textContent)
+    search(e.target.textContent);
 } );
-
-
-
-// cityButton.on("click", search());
-
-//retrieve data from local storage
-
-
-
 
 //future feature
 //clear search history button
